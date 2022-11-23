75af3b3af9af3295087f7bcf992d741adb518f84
0xdebaser <0xdebaser@gmail.com>
2022-11-22 15:07:53 -1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import twitterBtn from "./Twitter social icons - circle - white.svg";
+
+const API_URL = "https://api.quotable.io";
+const quotesArray = [];
 
 function App() {
+  const [activeQuote, setActiveQuote] = useState();
+
+  async function loadQuotes() {
+    const response = await fetch(`${API_URL}/quotes?limit=150`);
+    const data = await response.json();
+    quotesArray.push(...data.results);
+    getRandomQuote();
+  }
+
+  async function getRandomQuote() {
+    const randIndex = Math.floor(Math.random() * quotesArray.length);
+    setActiveQuote(quotesArray[randIndex]);
+  }
+
+  useEffect(() => {
+    loadQuotes();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div id="quote-box">
+        <div className="quote-component" id="text">
+          {activeQuote
+            ? `"${activeQuote.content}"`
+            : "Internet quotes are not trustworthy."}
+        </div>
+        <div className="quote-component" id="author">
+          {activeQuote ? `--${activeQuote.author}` : "--This site's dev"}
+        </div>
+        <button id="new-quote" onClick={getRandomQuote}>
+          New Quote
+        </button>
+        <br />
+        <a href="https://twitter.com/intent/tweet" id="tweet-quote">
+          <img src={twitterBtn} id="twitter-btn" />
         </a>
-      </header>
+      </div>
     </div>
   );
 }
